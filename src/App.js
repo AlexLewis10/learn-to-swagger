4713fa24a036cb730ec8c4b2145bae7894cc9775
Alex Lewis <alexlewis374@gmail.com>
2020-08-24 15:11:15 +0100

    Connect to petstore swagger.json in redocly component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { RedocStandalone } from 'redoc';
+
 
 function App() {
   return (
     <div>
       <p>Hello</p>
+      <RedocStandalone
+        specUrl="https://petstore.swagger.io/v2/swagger.json"
+        onLoaded={error => {
+          if (!error) {
+            console.log('Yay!');
+          }
+        }}
+      />
     </div>
   );
 }
